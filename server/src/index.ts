bfdeb22951875e3d5258eb0e90a09ef5571dd90e
pikokr <parangee9706@gmail.com>
2020-12-21 16:24:43 +0900

    backup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!')
+import 'graphql-import-node'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './typedefs.graphql'
+
+
+
+const app = express()
+
+
+const server=  new ApolloServer({
+    typeDefs
+})
