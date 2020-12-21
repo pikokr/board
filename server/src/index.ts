71993394e7a6f2ef1e88da5773948b012554b6c0
pikokr <parangee9706@gmail.com>
2020-12-21 16:37:18 +0900

    backup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,19 @@ import "graphql-import-node";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from "./typedefs.graphql";
-import resolvers from './resolvers'
-import config from '../config.json'
+import resolvers from "./resolvers";
+import config from "../config.json";
+import http from 'http'
 
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
-server.applyMiddleware({app, path: '/graphql'})
+server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen(config.server.port, () => console.log('Listening'))
+const httpServer = http.createServer(app)
+
+httpServer.listen({port: config.server.port}, () => console.log("Listening"));
