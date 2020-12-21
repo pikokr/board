40c45373d3cdb8f5a1e215be1bf69f739a66ba18
pikokr <parangee9706@gmail.com>
2020-12-21 16:28:05 +0900

    feat: listen

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
-import 'graphql-import-node'
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import typeDefs from './typedefs.graphql'
+import "graphql-import-node";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import typeDefs from "./typedefs.graphql";
+import resolvers from './resolvers'
+import config from '../config.json'
 
+const app = express();
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
-const app = express()
+server.applyMiddleware({app, path: '/graphql'})
 
-
-const server=  new ApolloServer({
-    typeDefs
-})
+app.listen(config.server.port, () => console.log('Listening'))
